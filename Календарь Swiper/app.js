8e23cb05b610e2e0ddbdeb43d9742ba4a2bd562b
OlesyaAsanova88 <asa88.88@bk.ru>
2022-02-25 17:19:16 +0300

    Add files via upload

---
+++ Календарь Swiper/app.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+   slidesPerView: 12,
+   grid: {
+      rows: 4,
+   },
+   spaceBetween: 5,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+   },
+})
+

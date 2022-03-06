adaa15a8c0a2d3310a725af59b72c424ab5f231a
OlesyaAsanova88 <asa88.88@bk.ru>
2022-03-06 13:11:45 +0300

    Add files via upload

---
+++ Vue.puma/main.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+   data() {
+      return {
+         like: 0,
+         animals: 'puma',
+         image: 'https://images.unsplash.com/photo-1643579296072-c78054cbb51b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDIyfEpwZzZLaWRsLUhrfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+         inventory: 100,
+         details: ['скорость', 'мощь', 'грация']
+      }
+   }
+})
+
